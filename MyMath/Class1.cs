3331d6bf15d488c12d80b51076e4ca0a725c8b06
SodaCris <18463922396@163.com>
2021-11-10 10:29:06 +0800

    init

---
+++ MyMath/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Class1
+    {
+    }
+
+    public class Rooter
+    {
+        public Rooter()
+        {
+        }
+
+        public double SquareRoot(double input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
